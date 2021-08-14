6b1688cddabdcac7cae25f6abcd6c011f00f5faa
konstantinos <kpetronikolos@gmail.com>
2021-08-14 18:12:52 +0300

    Connect React with Redux.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import Toasts from "./components/Toasts";
+import { addToast } from "./actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { addToast } = this.props.actions;
+    setTimeout(() => {
+      addToast({ text: "My Alert Message!" });
+    }, 120000)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <main>
+            <section>
+              <h1>Alert react redux app</h1>
+              <p>Click the button below to dispatch an alert message after 2 minutes.</p>
+              <button onClick={this.handleClick}>Dispatch</button>
+            </section>
+            <Toasts />
+          </main>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ addToast }, dispatch)
+});
+
+export default connect(null, mapDispatchToProps)(App);
