bf22da10faa58fc346aba5938a0021b9df5071a0
konstantinos <kpetronikolos@gmail.com>
2021-08-14 18:05:58 +0300

    Refactor Reducer.

--- src/reducers/toasts.js
+++ src/reducers/toasts.js
@@ -1,7 +1,20 @@
+import { ADD_TOAST, REMOVE_TOAST } from "../constants";
+
 export default function toasts(state = [], action) {
   const { payload, type } = action;
+  console.log(...state, 'state');
+  console.log(action, 'action');
 
   switch (type) {
+    case ADD_TOAST: {
+      console.log(payload, ...state, 'reducer type add_toast');
+      return [payload, ...state];
+    }
+
+
+    case REMOVE_TOAST:
+      return state.filter(toast => toast.id !== payload);
+
     default:
       return state;
   }
